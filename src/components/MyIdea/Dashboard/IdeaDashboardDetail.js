9307e877380192d2877eab0a64d6a444af7ab88f
dardecena <darlene.decena@gmail.com>
2019-04-17 22:04:29 +0200

    Change to flex box

--- src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
+++ src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
@@ -19,23 +19,12 @@ export default function IdeaDashboardDetail(props) {
             .then(res => setUserIdeas(res.body.idea))
     }, []);
 
-    const processTitle = (title) => {
-        let splitTitle = title.split('?')
-        const processedTitle = splitTitle[0] 
-        return processedTitle
-    }
-
     let qAnswers = []
     const qTitles = []
     userIdeas.map(idea => {
-        idea.answers.map(question => {
-            if (question.qTitle.length > 50) {
-            const title = processTitle(question.qTitle) 
-            qTitles.push(title)
-        } else {
+        idea.answers.map(question => 
             qTitles.push(question.qTitle)
-            }
-        })
+        )
     })
 
     userIdeas.map(idea => {
@@ -44,8 +33,6 @@ export default function IdeaDashboardDetail(props) {
         })
     })
 
-    
-
     qAnswers = qAnswers.map(answer => typeof answer === 'object' ? answer[0] ? answer[0].value : answer.value : answer)
 
     if (qAnswers[0] === 'true') {
@@ -53,43 +40,80 @@ export default function IdeaDashboardDetail(props) {
     }
 
     return (
-        
-        <Grid className='dashboard-container'
-            container
-            direction="row"
-            justify="space-evenly"
-            alignItems="flex-start"
-            >   
-            <div>
-                <br /><br /><br /><br /><br />
-                <StyledDiv>
-                    <h1>Assessing Your Idea:</h1>
-                    <ul className="step-progress">
-                        <li className="step-progress-item is-done"><strong>Submit your idea</strong></li>
-                        <li className="step-progress-item current"><strong>First patent check (1 week)</strong></li>
-                        <li className="step-progress-item"><strong>Expert check (2 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Second patent check (2 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Validation phase (4 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Final patent check (2 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Business plan phase (2 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Funding phase (2 weeks)</strong></li>
-                        <li className="step-progress-item"><strong>Company is born (1 week)</strong></li>
-                    </ul>
-                </StyledDiv>
-            </div>
-            <main>
-                <br /><br /><br /><br /><br /><br />
-                <h1 className='header'> Questions and Answers about Idea</h1>
-                { qTitles.map((title, index) => 
-                    <div key={index} className='questions-answers'>
-                        <StyledCard className='card-detail'>
-                            <h4>{title}:</h4>
-                            <p>{qAnswers[index]}</p>
-                        </StyledCard>
-                    </div>
-                )}
-            </main>
-        </Grid>
+        <div className="dashboard-container">
+        <Container>
+            <Left>
+                <FlexRow>
+                    <FlexColumn>
+                       
+                        <StyledDiv>
+                            <h1>Assessing Your Idea:</h1><hr/>
+                            <ul className="step-progress">
+                                <li className="step-progress-item is-done"><strong>Submit your idea</strong></li>
+                                <li className="step-progress-item current"><strong>First patent check (1 week)</strong></li>
+                                <li className="step-progress-item"><strong>Expert check (2 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Second patent check (2 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Validation phase (4 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Final patent check (2 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Business plan phase (2 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Funding phase (2 weeks)</strong></li>
+                                <li className="step-progress-item"><strong>Company is born (1 week)</strong></li>
+                            </ul>
+                        </StyledDiv>
+                    </FlexColumn>
+                </FlexRow>
+            </Left>
+            <Right>
+                <Content>
+                    <h1 className='header'> Questions and Answers about Idea:</h1>
+                    { qTitles.map((title, index) => 
+                        <div key={index}>
+                            <StyledCard>
+                                <h4>{title}:</h4>
+                                <p>{qAnswers[index]}</p>
+                            </StyledCard>
+                        </div>
+                    )}
+                </Content>
+            </Right>
+        </Container>
+        </div>
+        // <Grid className='dashboard-container'
+        //     container
+        //     direction="row"
+        //     justify="space-evenly"
+        //     alignItems="flex-start"
+        //     >   
+        //     <div>
+        //         <br /><br /><br /><br /><br />
+                // <StyledDiv>
+                //     <h1>Assessing Your Idea:</h1>
+                //     <ul className="step-progress">
+                //         <li className="step-progress-item is-done"><strong>Submit your idea</strong></li>
+                //         <li className="step-progress-item current"><strong>First patent check (1 week)</strong></li>
+                //         <li className="step-progress-item"><strong>Expert check (2 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Second patent check (2 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Validation phase (4 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Final patent check (2 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Business plan phase (2 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Funding phase (2 weeks)</strong></li>
+                //         <li className="step-progress-item"><strong>Company is born (1 week)</strong></li>
+                //     </ul>
+                // </StyledDiv>
+        //     </div>
+        //     <main>
+        //         <br /><br /><br /><br /><br /><br />
+                // <h1 className='header'> Questions and Answers about Idea</h1>
+                // { qTitles.map((title, index) => 
+                //     <div key={index} className='questions-answers'>
+                //         <StyledCard className='card-detail'>
+                //             <h4>{title}:</h4>
+                //             <p>{qAnswers[index]}</p>
+                //         </StyledCard>
+                //     </div>
+                // )}
+        //     </main>
+        // </Grid>
     )
 
 }
@@ -101,8 +125,66 @@ const StyledDiv = styled.div `
     font-size: 14px;
     border: 1px solid #ccc;
     padding: 20px;
-`
+    color: white;
+`;
 const StyledCard = styled(Card) `
-    background-color: rgb(255,255,255, 0.3);`
-
+    background-color: rgb(255,255,255, 0.3);
+    padding-left: 8px;
+    padding-right: 8px;
+`;
+const Left = styled.div`
+    grid-area: left;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: start;
+    align-items: flex-start;
+    padding-top: 100px;
+    padding-left: 30px;
+`;
     
+const FlexRow = styled.div`
+    display: flex;
+    @media only screen and (orientation:portrait) { 
+        flex-direction: column;
+    }
+`;
+
+const FlexColumn = styled.div`
+    display: flex;
+    flex: 1;
+`;
+
+const Content = styled.div`
+    align-self: center;
+    justify-self: center;
+    color: #ffffff;
+    width: 90vw;
+    max-width: 800px;
+    height: auto;
+    padding: 20px;
+    
+`;
+
+const Right = styled.div`
+    grid-area: right;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content:start;
+    align-items:flex-start;
+    padding-top:100px;
+`;
+
+const Container = styled.div`
+    width: 100vw;
+    
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: "left right";
+    
+	background-image: linear-gradient(to right top, #1a3d7c, #195d9c, #1f7fbb, #31a2d7, #4cc5f1);
+`;
